4b887b5d36895c49ec24b344aadc922f298b9a21
thzero <thzero@users.noreply.github.com>
2022-03-27 09:37:40 -0500

    code analyze updates

---
+++ GlobalSuppressions.cs
@@ -0,0 +1,17 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.temperingShiftU(System.UInt32)~System.UInt32")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.compute53BitRandom(System.Double,System.Double)~System.Double")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.init(System.UInt32)")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.init(System.UInt32[])")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.Next(System.Int32)~System.Int32")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.Next(System.Int32,System.Int32)~System.Int32")]
+[assembly: SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.NextUInt32(System.UInt32,System.UInt32)~System.UInt32")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.temperingShiftL(System.UInt32)~System.UInt32")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.temperingShiftS(System.UInt32)~System.UInt32")]
+[assembly: SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "<Pending>", Scope = "member", Target = "~M:thZero.MersenneTwister.temperingShiftT(System.UInt32)~System.UInt32")]
